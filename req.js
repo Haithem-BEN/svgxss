eff5b940e29f95bafbb8a51d0ba019e85ed4f18f
Bensalem Haithem <42184885+Haithem-BEN@users.noreply.github.com>
2023-03-01 02:22:24 +0100

    Update req.js

--- req.js
+++ req.js
@@ -1,6 +1,6 @@
 const Http = new XMLHttpRequest();
 const url='https://jsonplaceholder.typicode.com/posts';
-Http.open("GET", url);
+Http.open("POST", url);
 Http.send();
 
 Http.onreadystatechange = (e) => {
